64bd34aded42a64b2e738f92e98e15f40691e76b
gmaggiotti <gmaggiotti@gmail.com>
2017-03-24 01:03:46 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import JsxCompiler from './JsxCompiler';
+
+
+ReactDOM.render(
+  <JsxCompiler />,
+  document.getElementById('root')
+);
