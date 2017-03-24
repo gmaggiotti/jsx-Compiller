2b18e4d66428f57c9c67fc171658c3fd140edb9e
gmaggiotti <gmaggiotti@gmail.com>
2017-03-24 06:20:25 -0300

    Create js compiler logic

--- src/JsxCompiler.js
+++ src/JsxCompiler.js
@@ -1,15 +1,46 @@
 import React, { Component } from 'react';
 
 class JsxCompiler extends Component {
-  render() {
-    return (
-      <div className="JsxCompiler">
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+
+    constructor(){
+        super();
+        this.state = {
+            input: '/* add your jsx code here */',
+            output: '',
+            err: ''
+        }
+    }
+    update(e){
+        let code = e.target.value;
+        try {
+            this.setState({
+                output: window.Babel
+                    .transform(code, { presets: ['es2015', 'react']})
+                    .code,
+                err: ''
+            })
+        }
+        catch(err){
+            this.setState({err: err.message})
+        }
+    }
+    render(){
+        return (
+            <div>
+                <header>{this.state.err}</header>
+                <div className="container">
+          <textarea
+              onChange={this.update.bind(this)}
+              defaultValue={this.state.input}/>
+                    <pre>
+            {this.state.output}
+          </pre>
+                </div>
+            </div>
+        )
+    }
 }
+
+
 
 export default JsxCompiler;
